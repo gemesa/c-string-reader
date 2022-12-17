da197a57241a3889594e361f638c18b7031c0590
Andras Gemes <59890178+gemesa@users.noreply.github.com>
2022-12-17 20:27:09 +0100

    ci: add valgrind

---
+++ test/valgrind.sh
@@ -0,0 +1,25 @@
+valgrind --leak-check=full \
+  --show-leak-kinds=all \
+  --track-origins=yes \
+  --verbose \
+  --error-exitcode=1 \
+  --log-file="$(dirname "$0")/../artifacts/valgrind.log" \
+  "$(dirname "$0")/../artifacts/c-string-reader" >"$(dirname "$0")/../artifacts/c-string-reader.log"
+
+status=$?
+
+echo "valgrind.log:"
+cat "$(dirname "$0")/../artifacts/valgrind.log"
+
+echo "c-string-reader.log:"
+cat "$(dirname "$0")/../artifacts/c-string-reader.log"
+
+echo ""
+echo "summary:"
+if [ "$status" -eq 1 ]; then
+  echo "valgrind failed"
+  exit 1
+else
+  echo "valgrind passed"
+  exit 0
+fi
