4ba02c46a817af6585333c5dd2086462b6f79b57
Andras Gemes <59890178+gemesa@users.noreply.github.com>
2022-12-18 02:02:28 +0100

    feat: add argparser module

--- main.c
+++ main.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "modules/argparser/argparser.h"
 #include "modules/reader/reader.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-    char *filename = "test/input.txt";
-    FILE *file = fopen(filename, "r");
+    argparser_t *argparser = argparser_new();
+    argparser->parse(argparser, argc, argv);
 
-    if (file == NULL)
+    if (argparser->status == EXIT_FAILURE)
     {
-        perror(filename);
+        argparser->print(argparser);
+        argparser->free(argparser);
         return EXIT_FAILURE;
+    }
+
+    if (argparser->input != stdin)
+    {
+        argparser->input = fopen(argparser->filename, "r");
+
+        if (argparser->input == NULL)
+        {
+            perror(argparser->filename);
+            argparser->free(argparser);
+            return EXIT_FAILURE;
+        }
     }
 
     reader_t *reader = reader_new();
 
     puts("Enter strings:");
-    switch (reader->read(reader, file))
+    switch (reader->read(reader, argparser->input))
     {
         case EXIT_FAILURE_MAX_LEN_REACHED:
             puts("Invalid input (maximum length reached)");
@@ -33,8 +47,9 @@ int main(void)
     }
 
     reader->print(reader);
-    (void) fclose(file);
+    (void) fclose(argparser->input);
     reader->free(reader);
+    argparser->free(argparser);
 
     return EXIT_SUCCESS;
 }
